bb0d83f9b161a415661ebc273e5e6abb01e082da
Noé PHILIPPE <noephilippe29@gmail.com>
2022-06-09 00:30:26 +0200

    ✨Start project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 import React from 'react';
+import '../themes/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
