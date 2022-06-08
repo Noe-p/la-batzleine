f41287285d57e833393e3b11997e2ef04af685cf
Noé PHILIPPE <noephilippe29@gmail.com>
2022-06-09 00:31:20 +0200

    💄Create constants for style and add fonts

--- src/themes/constants.ts
+++ src/themes/constants.ts
@@ -4,3 +4,25 @@ export const MEDIA_QUERIES = {
   MOBILE_AND_TABLET: '(max-width: 1199px)',
   DESKTOP: '(min-width: 1200px)',
 };
+
+export const colors = {
+  black: 'rgb(10, 10, 10)',
+  blackTransparent: 'rgba(10, 10, 10, 1)',
+  white: 'rgb(255, 255, 255)',
+  spcaceCadet: 'rgb(43, 58, 103)',
+  spcaceCadetTransparent: 'rgba(43, 58, 103, 0.5)',
+};
+
+export const fontSizes = {
+  xxl: '100px',
+  xl: '40px',
+};
+
+export const fontFamily = {
+  abril: "'Abril Fatface', serif",
+  playfair: "'Playfair Display', serif",
+  urbanist: "'Urbanist', sans-serif",
+  cormorant: "'Cormorant', serif",
+  joan: "'Joan', serif",
+  nanum: "'Nanum Myeongjo', serif",
+};
